46281e7a4adb7cfaed55b48633582fb06253a9ab
azero_developer <azero_developer@soundai.com>
2019-11-15 14:41:56 +0800

    Update: Update README

--- src/main/java/com/azero/skill/helloworld/ServletInitializer.java
+++ src/main/java/com/azero/skill/helloworld/ServletInitializer.java
@@ -7,6 +7,8 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	System.setProperty(ServletConstants.TIMESTAMP_TOLERANCE_SYSTEM_PROPERTY, "3600000");
+        System.setProperty(ServletConstants.DISABLE_REQUEST_SIGNATURE_CHECK_SYSTEM_PROPERTY, "true");
         return application.sources(HelloworldApplication.class);
     }
 
